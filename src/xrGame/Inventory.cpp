dba0231eefc59265e3032c799d06143f654fc560
zcaliptium <zcaliptium@gmail.com>
2015-07-20 14:35:47 +0300

    XEM #90 - Bolt now works in MP.
    Because i need it and i want it!

--- src/xrGame/Inventory.cpp
+++ src/xrGame/Inventory.cpp
@@ -690,7 +690,9 @@ bool CInventory::Action(u16 cmd, u32 flags)
 	case kWPN_6:
 		{
 			b_send_event = true;
-			if (cmd == kWPN_6 && !IsGameTypeSingle()) return false;
+			// XEM #90 {
+			//if (cmd == kWPN_6 && !IsGameTypeSingle()) return false;
+			// XEM #90 }
 			
 			u16 slot = u16(cmd - kWPN_1 + 1);
 			if ( flags & CMD_START )
